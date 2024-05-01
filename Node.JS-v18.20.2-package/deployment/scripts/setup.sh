385612f7265b6f8dba5dfb35fda0e7d8a79ec24c
cpoulio <137225014+cpoulio@users.noreply.github.com>
2024-05-01 15:44:46 -0600

    Fixed Setup

--- Node.JS-v18.20.2-package/deployment/scripts/setup.sh
+++ Node.JS-v18.20.2-package/deployment/scripts/setup.sh
@@ -3,8 +3,6 @@
 # ${deploy_dir} is set by Ansible and represents the directory where the script is executed.
 # No need to set a default for ${deploy_dir} since it's provided by Ansible.
 # You have to have something (MODE for example ) to pass into the script so you can use the Options {install| uninstall| update}. Install is the default.
-#
-
 # **** IMPORTANT*****  MODE has to stand for NULL ("") so the if an Option is selecet then the it will excicute the default is install. ****
 if [[ ${MODE} = "" ]]; then
    MODE="install"
