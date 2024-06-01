1e9744667f233bb983bf4dd94e084dbebc99f3cb
cpoulio <137225014+cpoulio@users.noreply.github.com>
2024-06-01 17:23:50 -0600

    Add files via upload

---
+++ Node.JS-v18.20.3-package/deployment/scripts/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# ${deploy_dir} is set by Ansible and represents the directory where the script is executed.
+# No need to set a default for ${deploy_dir} since it's provided by Ansible.
+# You have to have something (MODE for example ) to pass into the script so you can use the Options {install| uninstall| update}. Install is the default.
+# **** IMPORTANT*****  MODE has to stand for NULL ("") so the if an Option is selecet then the it will excicute the default is install. ****
+if [[ ${MODE} = "" ]]; then
+   MODE="install"
+fi
+##################################################################################################################
+
+echo "MODE=${MODE}" # Testing to see if install or uninstall comes across
+echo "Deployment Directory=${deploy_dir}"  # Confirming the directory set by Ansible
+
+SCRIPT="${deploy_dir}/NodeJS.sh"
+#SCRIPT="./NodeJS.sh"
+
+case ${MODE} in
+    install)
+        echo "Switching to install mode..."
+        $SCRIPT install
+        ;;
+    uninstall)
+        echo "Switching to uninstall mode..."
+        $SCRIPT uninstall
+        ;;
+    *)
+        echo "Invalid mode. Usage: $0 {install|uninstall}"
+        exit 1
+        ;;
+esac
