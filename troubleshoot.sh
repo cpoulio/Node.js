31b02a643856534575a50288854d731cc6ecfdbe
cpoulio <137225014+cpoulio@users.noreply.github.com>
2025-02-12 12:11:13 -0700

    Update troubleshoot.sh

--- troubleshoot.sh
+++ troubleshoot.sh
@@ -9,3 +9,31 @@ capture_value() {
         shift
     done
 }
+
+# Parse Additional Arguments
+while [[ $# -gt 0 ]]; do
+    ARG="${1,,}"  # Convert argument to lowercase for case insensitivity
+    case "$ARG" in
+        --email|-e)
+            capture_value EMAIL "$@"
+            shift
+            ;;
+        --mode|-m)
+            capture_value MODE "$@"
+            shift
+            ;;
+        --om-server)
+            capture_value OM_SERVER "$@"
+            shift
+            ;;
+        --cert-server)
+            capture_value CERT_SERVER "$@"
+            shift
+            ;;
+        *)
+            echo "Invalid option: $1"
+            echo "Usage: $0 --mode {install|uninstall|activate|install_and_activate} [--email <email>] [--om-server <server>] [--cert-server <server>]"
+            exit 1
+            ;;
+    esac
+done
